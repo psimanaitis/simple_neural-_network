8fd265e774c1a02371dd09d6ccef62e61a73dc03
Povilas Simanaitis <mr.simanaitis@gmail.com>
2019-06-04 22:19:26 +0300

    improve code so it would use less hardcoded constants

--- demo.m
+++ demo.m
@@ -2,54 +2,47 @@ function simplest_neural_network
 
     training_input = [
     0 0 1,
-    1 1 1,
-    1 0 1,
-    0 1 1];
+    0 1 0,
+    0 1 1,
+    1 0 0,
+    1 1 0, 
+    1 0 1];
 
     traing_result = [
-    0, 1, 1 , 0    
+    0, 
+    0, 
+    0,
+    1,
+    1,
+    1
     ];
 
     weights = [
-    0.4, 0.3, 0.2    
+    rand , rand , rand 
     ];
 
     biasias = [
-    0.5, 0.4, 0.3    
+    rand, rand, rand    
     ];
 
-    testing_input = [
-    1, 0, 0    
-    ];
-
-    testing_output = [
-    1    
-    ];
-
+%     disp(weights );
+%     disp(biasias );
+    
     function T = sigmoid(x)
         T = 1 / (1 + exp(-x));
     end
     
-    function [weight_update, biasias_update] = calculate_updates(inputs, outputs, weights, biasias)
-          weight_updates = [
-               0 0 0, 
-               0 0 0, 
-               0 0 0,
-               0 0 0
-            ];
-
-            biasias_updates = [
-                0 0 0, 
-                0 0 0, 
-                0 0 0,
-                0 0 0
-            ];
+    function [weight_update, biasias_update, loss] = calculate_updates(inputs, outputs, weights, biasias)
+        input_samples_count = size(training_input,1);
+        weight_updates = zeros(input_samples_count, 3);
+        biasias_updates = zeros(input_samples_count, 3);
+        loss = zeros(input_samples_count, 3);
         
-          for t=1:1:4
+          for t=1:1:(input_samples_count -1)
             plain_results = inputs(t, :).*weights+biasias;
             normalized_result = sigmoid(sum(plain_results));
-%           loss = (outputs(t) - normalized_result)^2;
-
+            loss = (outputs(t) - normalized_result)^2;
+%             disp(loss);
             derivate_of_loss = 2*(outputs(t) - normalized_result);
             derivative_of_normalized_result = (exp(-t))./((1+exp(-t)).^2);
 
@@ -69,10 +62,24 @@ function simplest_neural_network
         biasias_update = (sum(biasias_updates)/3);
     end
 
-    for i=1:1:4
-        [weight_update, biasias_update] = calculate_updates(training_input, traing_result, weights, biasias);
-        weights = weights + weight_update;
-        biasias = biasias + biasias_update;
-    end    
 
+    for i=1:1:500
+        [weight_update, biasias_update, loss] = calculate_updates(training_input, traing_result, weights, biasias);
+        weights = weights + weight_update;
+        biasias = biasias + biasias_update;      
+%         disp(loss)
+    end    
+    
+%     disp(weights);
+%     disp(biasias);
+     
+    testing_input = [
+    1, 1, 1;
+    0, 0, 0
+    ];
+
+    testing_output = [
+    1,
+    0
+    ];
 end
